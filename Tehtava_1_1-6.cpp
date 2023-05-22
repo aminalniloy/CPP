e5583931d75401ce7f1b9c10b5d461099506399f
Al Amin <108260763+aminalniloy@users.noreply.github.com>
2023-05-22 12:39:07 +0300

    Add files via upload

---
+++ Tehtava_1_1-6.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	int ika;
+	int num1, num2, result;
+	cout << "Moi vaan kaikki!" << endl;
+	cout << "Python on kivaa" << endl;
+	cout << "Eiko vain?" << endl;
+	cout << "Sekuntien maara vuodessa:" << 365 * 24 * 60 * 60 << endl;
+	cout << "Kuinka vanha sina olet?" <<endl;
+	cin >> ika;
+	cout << "Olet " << ika << " vuotta vanha" <<endl;
+	cout << "Anna ensimainen luku: ";
+	cin >> num1;
+	cout << "Anna toinen luku: ";
+	cin >> num2;
+	result = num1 - num2;
+	cout << "Difference: " << result;
+
+
+
+
+
+	return 0;
+}
